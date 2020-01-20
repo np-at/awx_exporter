5e727c4103ddc38e74369b07ca9d6756bcad2579
np-at <59515761+np-at@users.noreply.github.com>
2020-01-19 16:41:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
 
     name='awx_exporter',
-    version='0.2',
+    version='0.2.1-beta',
     packages=['awx_exporter'],
     entry_points={
         "console_scripts": ['awx-export = awx_exporter.awx_exporter:main']
